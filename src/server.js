894a24bca8699980f93587fffcb599ca385e9b81
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2023-07-18 23:48:55 +0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('./config/index');
+const app = require('./app');
+async function main() {
+  try {
+    await mongoose.connect(config.database_url);
+
+    console.log(`🛢 Database is connected successfully`);
+
+    app.listen(config.port, () => {
+      console.log(`Application  listening on port : ${config.port}`);
+    });
+  } catch (err) {
+    console.log('Failed to connect database', err);
+  }
+}
+
+main();
+console.log('hi');
